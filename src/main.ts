b66f2313bca4372659ab3485ddd90828b89b59d9
Tomas Garcia <tomasotano25@gmail.com>
2022-10-07 14:52:54 -0400

    jsonplaceholder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
 
 async function loadUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch(import.meta.env.VITE_APP_API);
+  console.log(import.meta.env.VITE_APP_API);
   const data = await response.json();
   console.log(data);
 
@@ -11,7 +11,7 @@ async function loadUsers() {
   data.forEach((element: any) => {
     userList.innerHTML += `
       <div>
-        <h1>${element.name}</h1>
+        <h2>${element.name}</h2>
       </div>
     `;
   });
@@ -23,19 +23,7 @@ loadUsers();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <h1>Lista de usuarios</h1>
     <div id="users"></div>
   </div>
 `;
